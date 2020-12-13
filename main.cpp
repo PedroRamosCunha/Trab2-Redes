125ef375873c174cc20608a2cb47a60c4105e65e
PedroRamosCunha <pedrocunha@alunos.utfpr.edu.br>
2020-12-13 19:04:22 +0000

    CRC quase pronto

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include<string>
 #include <iostream>
+#include <time.h>
 #define SIZEQUADRO 288
-#define TIPODECONTROLE 0
+#define TIPODECONTROLE 2
 
 
 unsigned int resultadoParidade(char quadro[SIZEQUADRO / 8]) {
@@ -87,21 +88,45 @@ int ReceptorParidadePar(char quadro[SIZEQUADRO / 8]) {	//Analisa se resposta par
 }
 
 unsigned int CalcCRC(char quadro[SIZEQUADRO / 8]) {
-	unsigned int byte, crc, mask;
-	int i, j;
+	unsigned int res, poly_crc;
+	int i, j, pos;
 
-	i = 0;
-	crc = 0xFFFFFFFF;
-	while (quadro[i] != 0) {
-		byte = quadro[i];
-		crc = crc ^ byte;
+	//Dividendo 256 bits msg + 32 bits 0 -> Quadro
+	poly_crc = 0x82608EDB;//Polinomio de 33 bits sem o ultimo bit 1, para caber em um unsigned int
+	res = 0;//Resto
+
+	//1110 0011 1011 0111 0110 0010 0010 1000
+	//1110 0111 0111 0110 0111 1111 1001 1110
+
+	//Da certo
+	//1110 0000 1111 0011 1010 1101 0100 0000
+
+	std::cout << "Fim Quadro: " << quadro[4] << quadro[3] << quadro[2] << quadro[1] << quadro[0] << "\n";
+	
+	//Copia os primeiros quatro bytes para o res
+	for (i = 0; i < 4; i++) {
 		for (j = 7; j >= 0; j--) {
-			mask = crc & 1;
-			crc = (crc >> 1) ^ (0xEDB88320 & mask);
+			res = res << 1;
+			if ((quadro[i] >> j) % 2 != 0) {
+				res++;
+			}
 		}
-		i++;
 	}
-	return ~crc;
+
+	for(pos = 255; pos >= 0; pos--){
+		if(res >> 31 == 1){
+			res = res ^ poly_crc;
+		}
+
+		res = res << 1;
+		if(pos > 31 && (quadro[(SIZEQUADRO - pos)/8] >> ((SIZEQUADRO - pos)%8))%2 == 0){
+			res++;
+		}
+	}
+
+	std::cout << "CRC: " << res << "\n\n";
+
+	return res;
 }
 
 int ReceptorCRC(char quadro[SIZEQUADRO / 8]) {
@@ -112,7 +137,7 @@ int ReceptorCRC(char quadro[SIZEQUADRO / 8]) {
 	for (i = 4; i > 0; i--) {
 		for (j = 7; j >= 0; j--) {
 			crc = crc << 1;
-			if ((quadro[len - i] >> j) % 2 == 1 || (quadro[len - i] >> j) % 2 == -1) {
+			if ((quadro[len - i] >> j) % 2 != 0) {
 				crc++;
 			}
 		}
@@ -158,7 +183,7 @@ void MeioComunicacao(char fluxoBits[]) {
 		erro = 0;
 		for (j = 0; j < 8; j++) {
 			if ((rand() % 1000) < chance_erro) {//60% chance de não ter nenhum erro
-				erro++;
+				//erro++;
 			}
 			erro = erro << 1;
 		}
@@ -198,7 +223,7 @@ void TransmissorControleDeErro(char quadro[SIZEQUADRO / 8]) {
 		break;
 	}
 
-	std::cout << "32 bits escritos: " << res << std::endl;
+	//std::cout << "32 bits escritos: " << res << std::endl;
 
 
 
@@ -207,7 +232,7 @@ void TransmissorControleDeErro(char quadro[SIZEQUADRO / 8]) {
 	quadro[len - 2] = (res >> 8) % 256;
 	quadro[len - 1] = res % 256;
 
-	std::cout << (res >> 24) << " " << (res >> 16) % 256 << " " << (res >> 6) % 256 << " " << (res % 256) << std::endl;
+	//std::cout << (res >> 24) << " " << (res >> 16) % 256 << " " << (res >> 6) % 256 << " " << (res % 256) << std::endl;
 
 }
 
@@ -244,12 +269,12 @@ void CamApTr(std::string mensagem) {
 void AplicacaoTransmissora() {
 	std::string mensagem;
 	std::cout << "Qual mensagem você quer enviar?" << std::endl;
-	std::cin >> mensagem;
-
+	std::getline(std::cin, mensagem);	//Forma de receber a mensagem com espaços
 	CamApTr(mensagem);
 }
 
 int main(void) {
+  srand (time(NULL));
 	AplicacaoTransmissora();
 
 	return 0;

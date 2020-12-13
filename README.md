ca8b0e50fff4ccc2efc1258d149ed87f11384f7a
Gabriela Vidal <ga28bvidal@gmail.com>
2020-12-13 16:45:44 -0300

    Trabalho finalizado

--- README.md
+++ README.md
@@ -12,6 +12,66 @@ Ele foi desenvolvido para a matéria de SSC0641 - Redes de Computadores ministra
 ##Execução do código
 Para rodar o código, basta baixar o arquivo main.cpp e possuir um compilador de C++ (linguagem em que foi escrito o arquivo).
 
-Versão do Linux que foi desenvolvido o projeto: XXXXXX
+Dados do Compilador
 
-Compilador utilizado: XXXXXXX
+    ramos@Pedro:~$ gcc --version
+    gcc (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0
+    Copyright (C) 2019 Free Software Foundation, Inc.
+    This is free software; see the source for copying conditions.  There is NO
+    warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+Dados do Sistema Operacional
+
+    ramos@Pedro:~$ lsb_release -a
+    No LSB modules are available.
+    Distributor ID:	Ubuntu
+    Description:	Ubuntu 20.04.1 LTS
+    Release:	20.04
+    Codename:   focal
+
+Dados da máquina
+
+    ramos@Pedro:~$ lscpu
+    Architecture:                    x86_64
+    CPU op-mode(s):                  32-bit, 64-bit
+    Byte Order:                      Little Endian
+    Address sizes:                   48 bits physical, 48 bits virtual
+    CPU(s):                          4
+    On-line CPU(s) list:             0-3
+    Thread(s) per core:              2
+    Core(s) per socket:              2
+    Socket(s):                       1
+    NUMA node(s):                    1
+    Vendor ID:                       AuthenticAMD
+    CPU family:                      21
+    Model:                           101
+    Model name:                      AMD A10-9600P RADEON R5, 10 COMPUTE CORES 4C+6G
+    Stepping:                        1
+    Frequency boost:                 enabled
+    CPU MHz:                         1284.038
+    CPU max MHz:                     2400.0000
+    CPU min MHz:                     1200.0000
+    BogoMIPS:                        4791.03
+    Virtualisation:                  AMD-V
+    L1d cache:                       64 KiB
+    L1i cache:                       192 KiB
+    L2 cache:                        2 MiB
+    NUMA node0 CPU(s):               0-3
+
+
+    ramos@Pedro:~$sudo lshw -class CPU    
+    *-cpu                     
+       description: CPU
+       product: AMD A10-9600P RADEON R5, 10 COMPUTE CORES 4C+6G
+       vendor: Advanced Micro Devices [AMD]
+       physical id: 4
+       bus info: cpu@0
+       version: AMD A10-9600P RADEON R5, 10 COMPUTE CORES 4C+6G
+       serial: Unknown
+       slot: Socket FP4
+       size: 1325MHz
+       capacity: 2400MHz
+       width: 64 bits
+       clock: 100MHz
+       capabilities: lm fpu fpu_exception wp vme de pse tsc msr pae mce cx8 apic sep mtrr pge mca cmov pat pse36 clflush mmx fxsr sse sse2 ht syscall nx mmxext fxsr_opt pdpe1gb rdtscp x86-64 constant_tsc rep_good acc_power nopl nonstop_tsc cpuid extd_apicid aperfmperf pni pclmulqdq monitor ssse3 fma cx16 sse4_1 sse4_2 movbe popcnt aes xsave avx f16c lahf_lm cmp_legacy svm extapic cr8_legacy abm sse4a misalignsse 3dnowprefetch osvw ibs xop skinit wdt lwp fma4 tce nodeid_msr tbm topoext perfctr_core perfctr_nb bpext ptsc mwaitx cpb hw_pstate ssbd vmmcall fsgsbase bmi1 avx2 smep bmi2 xsaveopt arat npt lbrv svm_lock nrip_save tsc_scale vmcb_clean flushbyasid decodeassists pausefilter pfthreshold avic v_vmsave_vmload vgif overflow_recov cpufreq
+       configuration: cores=4 enabledcores=4 threads=4
--- main.cpp
+++ main.cpp
@@ -1,227 +1,280 @@
 #include<string>
 #include <iostream>
+#include <time.h>
 #define SIZEQUADRO 288
 #define TIPODECONTROLE 0
 
-void AplicRecep(std::string mensagem){	//Printa a mensagem recebida
-	std::cout <<"A mensagem recebida foi: "<< mensagem << std::endl;
+
+unsigned int resultadoParidade(char quadro[SIZEQUADRO / 8]) {
+	unsigned int res = 0;
+	int cont, i, j;
+	
+	//Calcula 1 bit de verificacao para cada char, total de 32 bits de paridade
+	for (i = 0;i < (SIZEQUADRO - 32) / 8;i++) {
+		cont = 0;
+		for (j = 0; j < 8;j++) {
+			if (((quadro[i] >> j) % 2) == 1) {
+				cont++;
+			}
+		}
+		if (cont % 2 == 0) {	//Se for par add 1
+			res++;
+		}
+		if (i != 31) {
+			res = (res << 1);
+		}
+	}
+	return res;
 }
 
-void camadaAplicRecep(char quadro[]){	//Converte quadro em mensagem
-	std::string mensagem=quadro;
+
+void AplicRecep(std::string mensagem) {	//Printa a mensagem recebida
+	std::cout << "A mensagem recebida foi:" << std::endl;
+
+	for (int i = 0; i < (SIZEQUADRO-32)/8; i++){
+		std::cout << mensagem[i];
+	}
+	std::cout << "\n";
+}
+
+void camadaAplicRecep(char quadro[SIZEQUADRO / 8]) {	//Converte quadro em mensagem
+	std::string mensagem = quadro;
+
+	mensagem.resize((SIZEQUADRO - 32) / 8);//Remove os bits de verificação
 
 	AplicRecep(mensagem);
 }
 
-void camadaEnlacRecep(char quadro[]){ //Recebe o quadro sem o controle de erro
-		camadaAplicRecep(quadro);
-}
 
-void ReceptorParidadeImpar(char quadro[]){	//Analisa se resposta paridade ímpar possui algum erro
-	int cont = 0;
-	int resposta;
-
-	for (int i=0; i<SIZEQUADRO; i++){
-		if(quadro[i]==1){
-			cont++;
-		}
-	}
-	if((cont%2==0 && quadro[SIZEQUADRO]==1) || (cont%2==1 && quadro[SIZEQUADRO]==0)){	//Se mensagem foi recebida corretamente
-		camadaEnlacRecep(quadro);
-	}else{	//se ocorreu um erro na transmissão
-		std::cout <<"Erro ao receber a mensagem" << std::endl;
-	}
-}
-
-void ReceptorParidadePar(char quadro[]){	//Analisa se resposta paridade par possui algum erro
+int ReceptorParidadeImpar(char quadro[SIZEQUADRO / 8]) {	//Analisa se resposta paridade ímpar possui algum erro
+	//Faz o calculo dos bits de paridade par e inverte o resultado
+	unsigned int resposta = ~resultadoParidade(quadro);
+	unsigned int comparacao = 0;
+	int len = SIZEQUADRO / 8;
 	int i, j;
-	int k=(SIZEQUADRO-32)/sizeof(int)-1;
-	int cont = 0;
-	unsigned int resposta;
-	int flag=0;		//flag para saber se teve algum erro
 
-	for(i=0; i<(SIZEQUADRO-32)/sizeof(int); i+=4){
-		cont=0;
-		for(j=i;j<i+4;j++){
-			if(quadro[i]==1){
-				cont++;
+	//Copia os bits de verificação em comparacao
+	for (i = 4; i > 0; i--) {
+		for (j = 7; j >= 0; j--) {
+			comparacao = comparacao << 1;
+			if ((quadro[len - i] >> j) % 2 == 1 || (quadro[len - i] >> j) % 2 == -1) {
+				comparacao++;
 			}
 		}
-		if(cont%2==0 && (resposta>>k)==){	//Se for par add 1
-			resposta++;
-		}
-		if(i!=255){
-			resposta=(resposta<<1);
-		}
-		k--;
 	}
+
+	if (resposta != comparacao) {	//Se é diferente, então a mensagem veio com erro
+		std::cout << "Mensagem recebida não está de acordo com a mensagem enviada!" << std::endl;
+		return -1;	//Retorno que indica que houve erro
+	}
+	return 0;	//retorno que indica que mensagem foi recebida sem erros
+}
+
+int ReceptorParidadePar(char quadro[SIZEQUADRO / 8]) {	//Analisa se resposta paridade par possui algum erro
+	//Faz o calculo dos bits de paridade par
+	unsigned int resposta = resultadoParidade(quadro);
+	unsigned int comparacao = 0;
+	int len = SIZEQUADRO / 8;
+	int i, j;
+
+	//Copia os bits de verificação em comparacao
+	for (i = 4; i > 0; i--) {
+		for (j = 7; j >= 0; j--) {
+			comparacao = comparacao << 1;
+			if ((quadro[len - i] >> j) % 2 == 1 || (quadro[len - i] >> j) % 2 == -1) {
+				comparacao++;
+			}
+		}
+	}
+
+	if (resposta != comparacao) {	//Se é diferente, então a mensagem veio com erro
+		std::cout << "Mensagem recebida não está de acordo com a mensagem enviada!" << std::endl;
+		return -1;	//Retorno que indica que houve erro
+	}
+	return 0;	//retorno que indica que mensagem foi recebida sem erros
+}
+
+unsigned int CalcCRC(char quadro[SIZEQUADRO / 8]) {
+	unsigned int res, poly_crc;
+	int i, j, pos;
+
+	//Quadro -> Dividendo 256 bits msg + 32 bits 0
 	
+	poly_crc = 0x82608EDB;//Polinomio de 33 bits sem o ultimo bit 1, para caber em unsigned int
+	res = 0;//Resto
+	
+	//Copia os primeiros quatro bytes para o res
+	for (i = 0; i < 4; i++) {
+		for (j = 7; j >= 0; j--) {
+			res = res << 1;
+			if ((quadro[i] >> j) % 2 != 0) {
+				res++;
+			}
+		}
+	}
+
+	//Faz a divisão de polinomio
+	for(pos = 255; pos >= 0; pos--){
+		if(res >> 31 == 1){
+			res = res ^ poly_crc;
+		}
+		res = res << 1;
+		
+		//pos > 32 ignora os bits de verificacao
+
+		//Como o ultimo bit do polinomio eh 1,
+		//o resultado do xor sera 1 apenas se o bit na mensagem for 0
+		if(pos > 32 && (quadro[(SIZEQUADRO - pos)/8] >> ((SIZEQUADRO - pos)%8))%2 == 0){
+			res++;
+		}
+	}
+
+	return res;
 }
 
-void ReceptorCRC(int quadro[]){
-  char CRC[31] = GeradorCRC(quadro[SIZEQUADRO]);
-  cont = 0;
-  for(int i=0, i<32, i++)
-  {
-  if(CRC[i]!=quadro[SIZEQUADRO-32+i])
-  {cont++}
-  }
-  if(cont>0) //Caso tenha algum erro cont>0
-  {std::cout <<"Erro ao receber a mensagem" << std::endl;}
+int ReceptorCRC(char quadro[SIZEQUADRO / 8]) {
+	int len = SIZEQUADRO / 8;
+	unsigned int crc = 0;
+	int i, j;
 
-}
+	//Copia os bits de verificação em CRC
+	for (i = 4; i > 0; i--) {
+		for (j = 7; j >= 0; j--) {
+			crc = crc << 1;
+			if ((quadro[len - i] >> j) % 2 != 0) {
+				crc++;
+			}
+		}
+	}
 
-void ReceptorControleDeErro (char quadro[]){	//Recebe o quadro com controle de erro que transmissor enviou
-	int tipoDeControleDeErro = TIPODECONTROLE;
-	switch (tipoDeControleDeErro) {
-		case 0:	//Controle de erro bit de paridade par
-			ReceptorParidadePar(quadro);
-			break;
-		case 1:	//Controle de erro bit de paridade ímpar
-			ReceptorParidadeImpar(quadro);
-			break;
-		case 2:	//Controle de erro CRC
-			ReceptorCRC(quadro);
-			break;
+	if (crc == CalcCRC(quadro)) {//Sem erro
+		return 0;
+	}
+	else {//Com erro
+		std::cout << "Mensagem recebida não está de acordo com a mensagem enviada!" << std::endl;
+		return -1;
 	}
 }
 
-void MeioComunicacao(char fluxoBits[]){
-	int chance_erro = 1;
-	int i,j;
-	int erro;
+void ReceptorControleDeErro(char quadro[SIZEQUADRO / 8]) {	//Recebe o quadro com controle de erro que transmissor enviou
+	int tipoDeControleDeErro = TIPODECONTROLE;
+	int res;
+	switch (tipoDeControleDeErro) {
+	case 0:	//Controle de erro bit de paridade par
+		res = ReceptorParidadePar(quadro);
+		break;
+	case 1:	//Controle de erro bit de paridade ímpar
+		res = ReceptorParidadeImpar(quadro);
+		break;
+	case 2:	//Controle de erro CRC
+		res = ReceptorCRC(quadro);
+		break;
+	}
+}
 
-	for(i=0; i<(SIZEQUADRO/sizeof(int)); i++){
-		erro=0;
-		for(j=0; j<8; j++){
-			if(rand()%1000<chance_erro){//75% chance de não ter nenhum erro
+void camadaEnlacRecep(char quadro[SIZEQUADRO / 8]) {
+	ReceptorControleDeErro(quadro);	//Chama a função que analisa se houve um erro
+
+	camadaAplicRecep(quadro);	//Chama a camada de aplicação da receptora
+}
+
+void MeioComunicacao(char fluxoBits[]) {
+	int chance_erro = 2;
+	int i, j;
+	char erro;
+
+	for (i = 0; i < (SIZEQUADRO / 8); i++) {
+		erro = 0;
+		for (j = 0; j < 8; j++) {
+			if ((rand() % 1000) < chance_erro) {//60% chance de não ter nenhum erro
+				std::cout << "Erro no bit " << 8*i+j << "\n";
 				erro++;
 			}
 			erro = erro << 1;
 		}
 		fluxoBits[i] = erro ^ fluxoBits[i];
 	}
+
+	camadaEnlacRecep(fluxoBits);
 }
 
-unsigned int TransmissorParidadePar (char quadro[]) {	//Cálculo para paridade par
-	int i, j;
-	int cont = 0;
-	unsigned int resposta;
-
-	for(i=0; i<(SIZEQUADRO-32)/sizeof(char); i+=4){
-		cont=0;
-		for(j=i;j<i+4;j++){
-			if(quadro[i]==1){
-				cont++;
-			}
-		}
-		if(cont%2==0){	//Se for par add 1
-			resposta++;
-		}
-		if(i!=255){
-			resposta=(resposta<<1);
-		}
-	}
+unsigned int TransmissorParidadePar(char quadro[SIZEQUADRO / 8]) {	//Cálculo para paridade par
+	unsigned int resposta = resultadoParidade(quadro);
 	return resposta;
 }
 
-unsigned int TransmissorParidadeImpar (char quadro[]) {	//Cálculo para paridade ímpar
-	int i, j;
-	int cont = 0;
-	unsigned int resposta;
-
-	for(i=0; i<(SIZEQUADRO-32)/sizeof(char); i+=4){
-		cont=0;
-		for(j=i;j<i+4;j++){
-			if(quadro[i]==1){
-				cont++;
-			}
-		}
-		if(cont%2==1){	//Se for ímpar add 1
-			resposta++;
-		}
-		if(i!=255){
-			resposta=(resposta<<1);
-		}
-	}
-	return resposta;
+unsigned int TransmissorParidadeImpar(char quadro[SIZEQUADRO / 8]) {	//Cálculo para paridade ímpar
+	unsigned int resposta = resultadoParidade(quadro);
+	return ~resposta;
 }
 
-unsigned int TransmissorCRC (char quadro[]) {
-	unsigned int byte, crc, mask;
-	int i,j;
-
-	i=0;
-	crc = 0xFFFFFFFF;
-	while (quadro[i] != 0){
-		byte = quadro[i];
-		crc = crc ^ byte;	
-		for(j = 7; j <= 0; j--){
-			mask = -(crc & 1);
-			crc = (crc >> 1) ^ (0xEDB88320 & mask);
-		}
-		i++;
-	}
-	return ~crc;
+unsigned int TransmissorCRC(char quadro[SIZEQUADRO / 8]) {
+	return CalcCRC(quadro);
 }
 
-void TransmissorControleDeErro (char quadro[]){
+void TransmissorControleDeErro(char quadro[SIZEQUADRO / 8]) {
 	int tipoDeControleDeErro = TIPODECONTROLE;
-	int len = SIZEQUADRO/8;
+	int len = SIZEQUADRO / 8;
 	unsigned int res;
 	switch (tipoDeControleDeErro) {		//Vê qual o controle de erro feito
-		case 0:	//Controle de erro de bit de paridade par
-			res = TransmissorParidadePar(quadro);
-			break;
-		case 1:	//Controle de erro de bit de paridade ímpar
-			res = TransmissorParidadeImpar(quadro);
-			break;
-		case 2:	//Controle de erro CRC
-			res = TransmissorCRC(quadro);
-			break;
+	case 0:	//Controle de erro de bit de paridade par
+		res = TransmissorParidadePar(quadro);
+		break;
+	case 1:	//Controle de erro de bit de paridade ímpar
+		res = TransmissorParidadeImpar(quadro);
+		break;
+	case 2:	//Controle de erro CRC
+		res = TransmissorCRC(quadro);
+		break;
 	}
 
-	quadro[len-4] += res >> 8*3;
-	quadro[len-3] += res >> 8*2;
-	quadro[len-2] += res >> 8;
-	quadro[len-1] += res;
+	quadro[len - 4] = res >> 24;
+	quadro[len - 3] = (res >> 16) % 256;
+	quadro[len - 2] = (res >> 8) % 256;
+	quadro[len - 1] = res % 256;
 }
 
-void camadaEnlaceTransm (char quadro[]){	//Camada de enlace chama a função que gera o controle de erros
+void camadaEnlaceTransm(char quadro[SIZEQUADRO / 8]) {	//Camada de enlace chama a função que gera o controle de erros
 	TransmissorControleDeErro(quadro);
+	MeioComunicacao(quadro);
 }
 
-void CamApTr(std::string mensagem){
-	int chars_available = (SIZEQUADRO-32)/8;
+void CamadaAplicacaoTransm(std::string mensagem) {
+	int chars_available = (SIZEQUADRO - 32) / 8;
 	int len = mensagem.length();
-	char quadro[SIZEQUADRO/8];
+	char quadro[SIZEQUADRO / 8];
 	int chars_left = len;
-	int i,j;
+	int i, j;
 
-	
-	for(i=0; chars_left>0; i++){
-		for(j=0; j<chars_available && j<chars_left; j++){
-			quadro[j] = mensagem[(i*chars_available)+j];
+	for (j = 0; j < (SIZEQUADRO / 8); j++) {	//Seta todos os bits do quadro como 0
+		quadro[j] = 0;
+	}
+
+	for (i = 0; chars_left > 0; i++) {	//Passar a strint mensagem para quadro
+	//Caso a mensagem tenha mais de 32 char, ela é dividida em várias e mandada em pacotes de 32 char
+		for (j = 0; j < chars_available && j < chars_left; j++) {
+			quadro[j] = mensagem[(i * chars_available) + j];
 		}
-		
+
 		camadaEnlaceTransm(quadro);
 
-		chars_left-=chars_available;
-		for(j=0; j<(SIZEQUADRO/8); j++){
-			quadro[j]=0;
+		chars_left -= chars_available;
+		for (j = 0; j < (SIZEQUADRO / 8); j++) {
+			quadro[j] = 0;
 		}
 	}
 }
 
-void AplicacaoTransmissora(void){
+void AplicacaoTransmissora() {
 	std::string mensagem;
-	std::cout <<"Qual mensagem você quer enviar?" << std::endl;
-	std::cin >>mensagem;
-	
-	CamApTr(mensagem);
+	std::cout << "Qual mensagem você quer enviar?" << std::endl;
+	std::getline(std::cin, mensagem);	//Forma de receber a mensagem com espaços
+	std::cout<<"\n";
+	CamadaAplicacaoTransm(mensagem);
 }
 
-int main (void){
+int main(void) {
+  srand (time(NULL));
 	AplicacaoTransmissora();
 
 	return 0;
